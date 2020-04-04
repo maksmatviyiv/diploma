1d28880e839d8530178068eae9fe38c5c4cae9f9
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2020-04-04 18:20:30 +0300

    convert Stateless to Stateful Widget, added validation for registration form, added expression for check  exist account or not

--- lib/components/registration.dart
+++ lib/components/registration.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../services/firebase_functionality.dart';
 import '../screens/indicators_screen.dart';
 
-class Registration extends StatelessWidget {
-  FirebaseFunctionality firebase = FirebaseFunctionality();
-  String email;
-  String password;
+class Registration extends StatefulWidget {
   final String titleRegistration;
 
   Registration({this.titleRegistration});
+
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+
+  FirebaseFunctionality _firebase = FirebaseFunctionality();
+  String _email;
+  String _password;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +26,7 @@ class Registration extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text(
-          titleRegistration,
+          widget.titleRegistration,
           style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
         SizedBox(
@@ -25,24 +34,41 @@ class Registration extends StatelessWidget {
         ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(hintText: 'Email'),
-                onChanged: (val) {
-                  email = val;
-                  print(email);
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Password"),
-                obscureText: true,
-                onChanged: (val) {
-                  password = val;
-                },
-              ),
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(hintText: 'Email'),
+                  onChanged: (value) {
+                    _email = value;
+                  },
+                  validator: (value) {
+                    bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value);
+                    if (emailValid == false) {
+                      return "Please input correct email";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Password"),
+                  obscureText: true,
+                  onChanged: (value) {
+                    _password = value;
+                  },
+                  validator: (value) {
+                    if (value.length < 8) {
+                      return "Password must contains more than 8 char";
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         SizedBox(
@@ -51,11 +77,23 @@ class Registration extends StatelessWidget {
         ButtonTheme(
           minWidth: 250.0,
           child: RaisedButton(
-            onPressed: () {
-              firebase.registration(email: email, password: password);
+            onPressed: () async {
+              if (_formKey.currentState.validate()) {
+                _firebase.registration(email: _email, password: _password);
+                if (_firebase.existAcount() != null) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                    _firebase.existAcount(),
+                    textAlign: TextAlign.center,
+                  )));
+                } else {
+                  print(_firebase.existAcount());
+                  Navigator.pushNamed(context, IndicatorsScreen.id);
+                }
+              }
             },
             color: Colors.green,
-            child: Text(titleRegistration),
+            child: Text(widget.titleRegistration),
           ),
         )
       ],
