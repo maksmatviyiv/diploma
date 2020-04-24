203c53c59b5bb29cd904a1dc6b56a27344f8ce3f
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2020-04-24 19:03:53 +0300

    added bottom navigation

--- lib/screens/indicators_screen.dart
+++ lib/screens/indicators_screen.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:smart_socket/constants.dart';
 
-class IndicatorsScreen extends StatelessWidget {
+import 'device_screen.dart';
+import 'scanner_screen.dart';
+
+class IndicatorsScreen extends StatefulWidget {
   static String id = "indicators_screen";
+
+  @override
+  _IndicatorsScreenState createState() => _IndicatorsScreenState();
+}
+
+class _IndicatorsScreenState extends State<IndicatorsScreen> {
+
+  int _currentIndex = 0;
+  List<Widget> _screens = [DeviceScreen(), ScannerScreen()];
+
+  void onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello"),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.green,
+        onTap: onTapped,
+        
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text(Strings.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            title: Text(Strings.addDevice)
+          ),
+        ],
       ),
     );
   }
