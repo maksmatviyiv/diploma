c2fbf7852ccb74af294fef8eb0de9ced341066f5
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2020-04-24 19:02:57 +0300

    added routes for Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,22 @@ import 'package:flutter/material.dart';
 import 'screens/registration_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/indicators_screen.dart';
+import 'screens/device_screen.dart';
 
 void main() => runApp(SmartSocket());
 
 class SmartSocket extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
       initialRoute: RegistrationScreen.id,
       routes: {
         RegistrationScreen.id : (context) => RegistrationScreen(),
         LoginScreen.id : (context) => LoginScreen(),
-        IndicatorsScreen.id :(context) => IndicatorsScreen()
+        IndicatorsScreen.id :(context) => IndicatorsScreen(),
+        DeviceScreen.id :(context) => DeviceScreen()
       },
     );
   }
