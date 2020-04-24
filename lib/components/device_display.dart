69d3c8db9d1d62fc4625b6269f71be3149c46b0f
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2020-04-24 20:10:29 +0300

    added divider between devices

--- lib/components/device_display.dart
+++ lib/components/device_display.dart
@@ -15,77 +15,86 @@ class DeviceDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: GestureDetector(
-          onTap: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text(Strings.manageDevice),
-                    content: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 3,
-                          child: TextField(
-                            decoration: InputDecoration(hintText: Strings.hintChangeName),
-                            onChanged: (newName) {
-                              newDeviceName = newName;
-                            },
-                          ),
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text(Strings.manageDevice),
+                        content: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 3,
+                              child: TextField(
+                                decoration: InputDecoration(hintText: Strings.hintChangeName),
+                                onChanged: (newName) {
+                                  newDeviceName = newName;
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: GestureDetector(
+                                onTap: () {
+                                  firebaseFunctionality.changeName(
+                                      documentId, newDeviceName);
+                                  Navigator.of(context).pop();
+                                },
+                                child: Icon(
+                                  Icons.edit_attributes,
+                                  size: 35.0,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: GestureDetector(
+                        actions: [
+                          GestureDetector(
                             onTap: () {
-                              firebaseFunctionality.changeName(
-                                  documentId, newDeviceName);
+                              firebaseFunctionality.deleteDevice(documentId);
                               Navigator.of(context).pop();
                             },
-                            child: Icon(
-                              Icons.edit_attributes,
-                              size: 35.0,
-                              color: Colors.green,
+                            child: Container(
+                              color: Colors.red,
+                              child: Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                                size: 35.0,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    actions: [
-                      GestureDetector(
-                        child: Container(
-                          color: Colors.red,
-                          child: Icon(
-                            Icons.delete,
-                            color: Colors.white,
-                            size: 35.0,
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                });
+                        ],
+                      );
+                    });
+              },
+              child: Icon(Icons.edit)),
+          title: Text(deviceName),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(temperature.toString()),
+              Switch(
+                activeColor: Colors.green,
+                inactiveThumbColor: Colors.red,
+                inactiveTrackColor: Colors.red,
+                value: status,
+                onChanged: (bool newValue) {
+                  firebaseFunctionality.turnOnOffDevice(documentId, newValue);
+                },
+              )
+            ],
+          ),
+          onTap: () {
+            print(deviceName);
           },
-          child: Icon(Icons.edit)),
-      title: Text(deviceName),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(temperature.toString()),
-          Switch(
-            activeColor: Colors.green,
-            inactiveThumbColor: Colors.red,
-            inactiveTrackColor: Colors.red,
-            value: status,
-            onChanged: (bool newValue) {
-              firebaseFunctionality.turnOnOffDevice(documentId, newValue);
-            },
-          )
-        ],
-      ),
-      onTap: () {
-        print(deviceName);
-      },
+        ),
+        Divider(color: Colors.black,),
+      ],
     );
   }
 }
