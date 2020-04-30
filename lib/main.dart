c3634cfbe94641f7f03fc02ea0e9d797d22b1fbd
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2020-04-30 19:19:20 +0300

    set possible screen orientation application

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/registration_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/indicators_screen.dart';
 import 'screens/device_screen.dart';
 
-void main() => runApp(SmartSocket());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(SmartSocket());
+}
 
 class SmartSocket extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: DeviceScreen.id,
+      initialRoute: IndicatorsScreen.id,
       routes: {
-        RegistrationScreen.id : (context) => RegistrationScreen(),
-        LoginScreen.id : (context) => LoginScreen(),
-        IndicatorsScreen.id :(context) => IndicatorsScreen(),
-        DeviceScreen.id :(context) => DeviceScreen()
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        IndicatorsScreen.id: (context) => IndicatorsScreen(),
+        DeviceScreen.id: (context) => DeviceScreen()
       },
     );
   }
