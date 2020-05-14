4d60d360e03687ee6c0406656ce1fd957bf8d284
maksmatviyiv <maskym.matviyiv1998@gmail.com>
2020-05-14 12:58:26 +0300

    added check, previously login user or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'screens/registration_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/indicators_screen.dart';
 import 'screens/device_screen.dart';
+import 'screens/indicators_screen.dart';
+import 'screens/registration_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +18,17 @@ class SmartSocket extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: IndicatorsScreen.id,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return IndicatorsScreen();
+          }
+          return RegistrationScreen();
+        },
+      ),
       routes: {
         RegistrationScreen.id: (context) => RegistrationScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
         IndicatorsScreen.id: (context) => IndicatorsScreen(),
         DeviceScreen.id: (context) => DeviceScreen()
       },
